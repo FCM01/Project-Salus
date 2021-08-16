58a363f1b947fc663c4806c19ccefc780303a553
FCM01 <faraimatyukira1@gmail.com>
2021-08-16 22:35:30 +0200

    api_bass

--- API.py
+++ API.py
@@ -14,50 +14,281 @@ def parse_json(data):
 
 
 app=Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/School"
 
 mongo  = PyMongo(app)
 CORS(app)
 
 
-@app.route("/User/Signup",metheds=["POST"])
+@app.route("/Admin/Signup",metheds=["POST"])
 def signup():
     status= 200
     resp ={}
     try:
-        username = request.json.get("username")
+        name = request.json.get("name")
+        surname = request.json.get("surname")
+        id_number = request.json.get("id_number")
+        date_of_birth = request.json.get("date_of_birth")
         email = request.json.get("email")
+        phone_number= request.json.get("phone_number")
+        address= request.json.get("address")
         password = request.json.get("password")
-        user_type= request.json.get("user_type")
-        if username!= "" and email !="" and password !="" and user_type != "":
+        if name!= "" and email !="" and password !="" and   id_number  != "":
+            uuid_number = tools()
+            admin_number = uuid_number.id_number_genrator() 
             signup_payload = {
-                "username":f"{username}",
+                "name":f"{name}",
+                "surnmae":f"{surname}",
+                "id_number":f"{id_number}",
+                "date_of_birth": f"{date_of_birth}",
                 "email":f"{email}",
-                "pasword":f"{password}",
-                "user_type":f"{user_type}"
+                "phone_number":f"{phone_number}",
+                "address":f"{address}",
+                "password":f"{password}",
+                "admin_number":f"{admin_number}",
+                "acess_level":0,
+                "token":[]
             }
-
-
-        
+            teacher  = mongo.db.teacher.insert(signup_payload)
+            status = 200
+            resp = {"message":"succeessful","status":f"{status}"}       
     except Exception as e:
         status = 400
         resp = {"message":"ERROR on /User/Signup","status":f"{status}",}
         print("ERROR:/User/Signup-->",e)
+    return jsonify(resp),status
+
+
+@app.route("/Teacher/Signup",metheds=["POST"])
+def signup():
+    status= 200
+    resp ={}
+    try:
+        name = request.json.get("name")
+        surname = request.json.get("surname")
+        id_number = request.json.get("id_number")
+        date_of_birth = request.json.get("date_of_birth")
+        email = request.json.get("email")
+        phone_number= request.json.get("phone_number")
+        address= request.json.get("address")
+        password = request.json.get("password")
+        position= request .json.get("position")
+        subject= request.json.get("subject")
+        register_class = request.json.get("register_class")
+        if name!= "" and email !="" and password !="" and   id_number  != "":
+            uuid_number = tools()
+            staff_number = uuid_number.id_number_genrator() 
+            signup_payload = {
+                "name":f"{name}",
+                "surnmae":f"{surname}",
+                "id_number":f"{id_number}",
+                "date_of_birth": f"{date_of_birth}",
+                "email":f"{email}",
+                "phone_number":f"{phone_number}",
+                "address":f"{address}",
+                "password":f"{password}",
+                "staff_number":f"{staff_number}",
+                "position":f"{position}",
+                "subject":f"{subject}",
+                "register_class":f"{register_class}",
+                "acess_level":2,
+                "token":[]
+            }
+            teacher  = mongo.db.teacher.insert(signup_payload)
+            status = 200
+            resp = {"message":"succeessful","status":f"{status}"}       
+    except Exception as e:
+        status = 400
+        resp = {"message":"ERROR on /User/Signup","status":f"{status}",}
+        print("ERROR:/User/Signup-->",e)
+    return jsonify(resp),status
+
 
         
+@app.route("/Securtity/Signup",metheds=["POST"])
+def signup():
+    status= 200
+    resp ={}
+    try:
+        name = request.json.get("name")
+        surname = request.json.get("surname")
+        id_number = request.json.get("id_number")
+        date_of_birth = request.json.get("date_of_birth")
+        email = request.json.get("email")
+        phone_number= request.json.get("phone_number")
+        address= request.json.get("address")
+        password = request.json.get("password")
+        position= request .json.get("position")
+        petrol_sector = request.json.get("petrol_sector") 
+        if name!= "" and email !="" and password !="" and   id_number  != "":
+            uuid_number = tools()
+            staff_number = uuid_number.id_number_genrator() 
+            signup_payload = {
+                "name":f"{name}",
+                "surnmae":f"{surname}",
+                "id_number":f"{id_number}",
+                "date_of_birth": f"{date_of_birth}",
+                "email":f"{email}",
+                "phone_number":f"{phone_number}",
+                "address":f"{address}",
+                "password":f"{password}",
+                "staff_number":f"{staff_number}",
+                "position":f"{position}",
+                "petrol_sector":f"{petrol_sector}",
+                "acess_level":2,
+                "token":[]
+            }
+            teacher  = mongo.db.teacher.insert(signup_payload)
+            status = 200
+            resp = {"message":"succeessful","status":f"{status}"}       
+    except Exception as e:
+        status = 400
+        resp = {"message":"ERROR on /User/Signup","status":f"{status}",}
+        print("ERROR:/User/Signup-->",e)
+    return jsonify(resp),status
+
+
+@app.route("/Domestic/Signup",metheds=["POST"])
+def signup():
+    status= 200
+    resp ={}
+    try:
+        name = request.json.get("name")
+        surname = request.json.get("surname")
+        id_number = request.json.get("id_number")
+        date_of_birth = request.json.get("date_of_birth")
+        email = request.json.get("email")
+        phone_number= request.json.get("phone_number")
+        address= request.json.get("address")
+        password = request.json.get("password")
+        position= request .json.get("position")
+        job_title = request.json.get("job_title") 
+        if name!= "" and email !="" and password !="" and   id_number  != "":
+            uuid_number = tools()
+            staff_number = uuid_number.id_number_genrator() 
+            signup_payload = {
+                "name":f"{name}",
+                "surnmae":f"{surname}",
+                "id_number":f"{id_number}",
+                "date_of_birth": f"{date_of_birth}",
+                "email":f"{email}",
+                "phone_number":f"{phone_number}",
+                "address":f"{address}",
+                "password":f"{password}",
+                "staff_number":f"{staff_number}",
+                "position":f"{position}",
+                "job_title":f"{job_title}",
+                "acess_level":1,
+                "token":[]
+            }
+            teacher  = mongo.db.teacher.insert(signup_payload)
+            status = 200
+            resp = {"message":"succeessful","status":f"{status}"}       
+    except Exception as e:
+        status = 400
+        resp = {"message":"ERROR on /User/Signup","status":f"{status}",}
+        print("ERROR:/User/Signup-->",e)
+    return jsonify(resp),status
 
 
 
+@app.route("/Student/Signup",metheds=["POST"])
+def signup():
+    status= 200
+    resp ={}
+    try:
+        name = request.json.get("name")
+        surname = request.json.get("surname")
+        id_number = request.json.get("id_number")
+        date_of_birth = request.json.get("date_of_birth")
+        email = request.json.get("email")
+        phone_number= request.json.get("phone_number")
+        address= request.json.get("address")
+        password = request.json.get("password")
+        register_class = request.json.get("register_class")
+        subject_combo = request.json.get("subject_combo")
+        time_table = request.jso.get("time_table")
+        guardian_name  = request.json.get("guardian_name")
+        guardian_surname = request.json.get("guardian_surname")
+        gaurdian_email  = request.json.get(" gaurdian_email")
+        gaurdian_phone_number = request.json.get("gaurdian_phone_number")
+        
+        if name!= "" and email !="" and password !="" and   id_number  != "":
+            uuid_number = tools()
+            student_number = uuid_number.id_number_genrator() 
+            signup_payload = {
+                "name":f"{name}",
+                "surnmae":f"{surname}",
+                "id_number":f"{id_number}",
+                "date_of_birth": f"{date_of_birth}",
+                "email":f"{email}",
+                "phone_number":f"{phone_number}",
+                "address":f"{address}",
+                "password":f"{password}",
+                "student_number":f"{student_number}",
+                "register_class":f"{register_class}",
+                "subject_combo":f"{subject_combo}",
+                "time_table":time_table,
+                "guardian_name":f"{guardian_name}",
+                "guardian_surname":f"{guardian_surname}",
+                "gaurdian_email":f"{gaurdian_email}",
+                "gaurdian_phone_number":f"{gaurdian_phone_number}",
+                "acess_level":0,
+                "token":[]
+            }
+            teacher  = mongo.db.teacher.insert(signup_payload)
+            status = 200
+            resp = {"message":"succeessful","status":f"{status}"}       
+    except Exception as e:
+        status = 400
+        resp = {"message":"ERROR on /User/Signup","status":f"{status}",}
+        print("ERROR:/User/Signup-->",e)
+    return jsonify(resp),status
 
 
-
-
-
-
-
-
-
-
+@app.route("/Visitor/Signup",metheds=["POST"])
+def signup():
+    status= 200
+    resp ={}
+    try:
+        name = request.json.get("name")
+        surname = request.json.get("surname")
+        id_number = request.json.get("id_number")
+        date_of_birth = request.json.get("date_of_birth")
+        email = request.json.get("email")
+        phone_number= request.json.get("phone_number")
+        address= request.json.get("address")
+        password = request.json.get("password")
+        purpose_of_visit= request .json.get("purpose_of_visit")
+        time_in = request.json.get("time_in") 
+        time_out = request.json.get("time_out") 
+        if name!= "" and email !="" and password !="" and   id_number  != "":
+            uuid_number = tools()
+            visitor_number = uuid_number.id_number_genrator() 
+            signup_payload = {
+                "name":f"{name}",
+                "surnmae":f"{surname}",
+                "id_number":f"{id_number}",
+                "date_of_birth": f"{date_of_birth}",
+                "email":f"{email}",
+                "phone_number":f"{phone_number}",
+                "address":f"{address}",
+                "password":f"{password}",
+                "visitor_number":f"{visitor_number}",
+                "purpose_of_visit":f"{purpose_of_visit}",
+                "time_in":f"{time_in}",
+                "time_out":f"{time_out}",
+                "acess_level":0,
+                "token":[]
+            }
+            teacher  = mongo.db.teacher.insert(signup_payload)
+            status = 200
+            resp = {"message":"succeessful","status":f"{status}"}       
+    except Exception as e:
+        status = 400
+        resp = {"message":"ERROR on /User/Signup","status":f"{status}",}
+        print("ERROR:/User/Signup-->",e)
+    return jsonify(resp),status
 
 
 
--- microfunction.py
+++ microfunction.py
@@ -5,16 +5,134 @@ import os
 import json
 import imghdr
 from email.message import EmailMessage
-from flask_pymongo import PyMongo
 from datetime import datetime
+import uuid
 
 class tools :
-    def user_account_creation(self,signup_payload):
-        try:
-            if signup_payload["user_type"]=="Teacher":
-                
-            elif signup_payload["user_type"]=="Student":
-            elif signup_payload["user_type"] =="Vistor":
+        def id_number_genrator(self):
+            id_number = uuid.uuid1()
+            return id_number
 
-        except Exception as e:
-            print("ERROR:user_account_creation():--->",e)
+        def emailing_services(self,reciver_email,name,type,verification_password=""):
+            try:
+                
+                email_address  ="dummyjackson8@gmail.com"
+                email_password  ="dummy101@1"
+                email_recieve = reciver_email
+
+                if type == "appointment":
+                    msg = EmailMessage()
+                    msg['Subject'] = 'Appointment Made With Doctor'
+                    msg['From'] = email_address
+                    msg['To']= email_recieve
+                    msg.set_content ( 'You have set up an appointment with Dr.Megan Timm practise we will get back to you ass soon as possible Number: 011 893 2100')
+                    msg.add_alternative(f"""
+                        <!DOCTYPE html>
+                        <html>
+                            <body>
+                                <h1 style ="color:#96c8cc;">Appointment Made</h1> 
+                                <h2 style ="color:#96c8cc;">Thank you {name}</h2>
+                                <p>You have set up an appointment with Dr.Megan Timm's practise we will get back to you as soon as possible with a time slot for you to come Number: 011 893 2100</p>
+                                <zbr>
+                                <p>Address</p>
+                                <p>27 gannet street </p>
+                                <p>GSermiston</p>    
+                                <p>1428</p> 
+                                <p>South Africa</p>  
+                            </body>
+                        </html>
+                        """,subtype= "html")
+                    files = ["Screenshot 2021-07-11 220843.png","download.jpg"]
+                    for images in files:
+                        with open(f"C:/Users/farai/OneDrive/Documents/personal work/startup/Flow/resources/{images}","rb") as image :
+                            file_data = image.read()
+                            file_type = imghdr.what(image.name)
+                            file_name= image.name
+                        msg.add_attachment(file_data,maintype="image",subtype=file_type,filename =file_name)
+                
+                elif type == "signup":
+                    msg = EmailMessage()
+                    msg['Subject'] = 'Welcome to FLOW2'
+                    msg['From'] = email_address
+                    msg['To']= email_recieve
+                    msg.set_content ( 'Welcome to flow we happy to provide u a new way to bring ease to you doctor appointment making system')
+                    msg.add_alternative(f"""
+                        <!DOCTYPE html>
+                        <html>
+                            <body>
+                                <h1 style ="color:#96c8cc;">Account Made</h1> 
+                                <h2 style ="color:#96c8cc;">Thank you {name}</h2>
+                                <p>Welcome to flow we happy to provide u a new way to bring ease to you doctor appointment making system</p>
+                                <p>Please proceed to provide your medical information</p>
+                                <p> We care about your well being </p>
+                            </body>
+                        </html>
+                        """,subtype= "html")
+                    files = ["Screenshot 2021-07-11 220843.png","download.jpg"]
+                    for images in files:
+                        with open(f"C:/Users/farai/OneDrive/Documents/personal work/startup/Flow/resources/{images}","rb") as image :
+                            file_data = image.read()
+                            file_type = imghdr.what(image.name)
+                            file_name= image.name
+                        msg.add_attachment(file_data,maintype="image",subtype=file_type,filename =file_name)
+                
+                elif type == "forgot_passoword":
+                    print("forgot passowrd")
+                    msg = EmailMessage()
+                    msg['Subject'] = 'Forgot Your Password '
+                    msg['From'] = email_address
+                    msg['To']= email_recieve
+                    msg.set_content ( 'Welcome to flow we happy to provide u a new way to bring ease to you doctor appointment making system')
+                    msg.add_alternative(f"""
+                        <!DOCTYPE html>
+                        <html>
+                            <body>
+                                <h1 style ="color:#96c8cc;">Forgot your password</h1> 
+                                <h2 style ="color:#96c8cc;">Hello {name}</h2>
+                                <p>A sign in attempt requires further verification because we did not recognize your device. To complete the sign in, enter the verification code on the unrecognized device.</p>
+                                <p>Here is your verification code :{verification_password}</p>
+                                <p>Yours sincerly</p>
+                                <p>The FLOW2 team</p>
+                            </body>
+                        </html>
+                        """,subtype= "html")
+                    files = ["Screenshot 2021-07-11 220843.png","download.jpg"]
+                    for images in files:
+                        with open(f"C:/Users/farai/OneDrive/Documents/personal work/startup/Flow/resources/{images}","rb") as image :
+                            file_data = image.read()
+                            file_type = imghdr.what(image.name)
+                            file_name= image.name
+                        msg.add_attachment(file_data,maintype="image",subtype=file_type,filename =file_name)
+                elif type == "change_password" : 
+                    print("change_password")
+                    msg = EmailMessage()
+                    msg['Subject'] = 'Change Your Password '
+                    msg['From'] = email_address
+                    msg['To']= email_recieve
+                    msg.set_content ( 'Welcome to flow we happy to provide u a new way to bring ease to you doctor appointment making system')
+                    msg.add_alternative(f"""
+                        <!DOCTYPE html>
+                        <html>
+                            <body>
+                                <h1 style ="color:#96c8cc;">Account Made</h1> 
+                                <h2 style ="color:#96c8cc;">Thank you {name}</h2>
+                                <p>Welcome to flow we happy to provide u a new way to bring ease to you doctor appointment making system</p>
+                                <p> WE care about your well being </p>
+                            </body>
+                        </html>
+                        """,subtype= "html")
+                    files = ["Screenshot 2021-07-11 220843.png","download.jpg"]
+                    for images in files:
+                        with open(f"C:/Users/farai/OneDrive/Documents/personal work/startup/Flow/resources/{images}","rb") as image :
+                            file_data = image.read()
+                            file_type = imghdr.what(image.name)
+                            file_name= image.name
+                        msg.add_attachment(file_data,maintype="image",subtype=file_type,filename =file_name)
+                with smtplib.SMTP_SSL("smtp.gmail.com" ,465) as smtp:
+                    smtp.login(email_address,email_password)
+
+                    smtp.send_message(msg)
+                    print("Email has been sent")    
+                    return True    
+            except Exception as e :
+                print("[email_service] emailingServices() joining error:",e)
